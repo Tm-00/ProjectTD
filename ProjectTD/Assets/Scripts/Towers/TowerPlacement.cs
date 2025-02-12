27aed48f0bec45e6ff17ddbd107bcceeb5a9ad6f
Tm-00 <t.mitchell3@wlv.ac.uk>
2025-02-12 12:57:34 +0000

    Fully implemented initial unit placement

--- ProjectTD/Assets/Scripts/Towers/TowerPlacement.cs
+++ ProjectTD/Assets/Scripts/Towers/TowerPlacement.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class TowerPlacement : MonoBehaviour
 {
 
-    [SerializeField] 
-    private Camera playerCamera;
     
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private LayerMask placementCollideMask;
+    [SerializeField] private LayerMask placementCheckMask;
     private GameObject _unit;
+    
     
     // Update is called once per frame
     void Update()
@@ -16,15 +19,31 @@ public class TowerPlacement : MonoBehaviour
         if (_unit != null)
         {
             Ray r = playerCamera.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(r, out RaycastHit hitInfo, 500f))
+            RaycastHit hitInfo;
+            if (Physics.Raycast(r, out  hitInfo, 100f, placementCollideMask))
             {
                 _unit.transform.position = hitInfo.point;
             }
 
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0) && hitInfo.collider.gameObject != null)
             {
-                _unit = null;
+                if (hitInfo.collider.gameObject.CompareTag("CanPlace"))
+                {
+                    BoxCollider unitCollider = _unit.gameObject.GetComponent<BoxCollider>();
+                    unitCollider.isTrigger = true;
+                    
+                    Vector3 BoxCenter = _unit.gameObject.transform.position + unitCollider.center;
+                    Vector3 HalfExtents = unitCollider.size / 2;
+                    if (Physics.CheckBox(BoxCenter, HalfExtents, Quaternion.identity, placementCheckMask, QueryTriggerInteraction.Ignore))
+                    {
+                        unitCollider.isTrigger = true;
+                    }
+                    else
+                    {
+                        unitCollider.isTrigger = false;
+                        _unit = null;
+                    }
+                }
             }
         }
     }
