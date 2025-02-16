1e64f39cb568a1115207e04a0d3e18f90306ae0d
Tm-00 <t.mitchell3@wlv.ac.uk>
2025-02-16 10:08:30 +0000

    populated core node health script, added game over script

--- ProjectTD/Assets/Scripts/CoreNode/CNHealth.cs
+++ ProjectTD/Assets/Scripts/CoreNode/CNHealth.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class CNHealth : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject coreNode;
+    private static readonly int MaxHealth = 10;
+    private static int _currentHealth = 10;
+    private static readonly int Amount = 1;
+    public static bool Hit = false;
+    public static bool IsDead = false;
+    
+    private static int HealthHandler(bool takenDamage)
     {
-        
+        if (takenDamage)
+        {
+            // Reset the flag after handling the event
+            Hit = false;
+            _currentHealth -= Amount;
+            Debug.Log("Core Node Health: "+ _currentHealth + "/" +  MaxHealth);
+        }
+        return _currentHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    public static bool CoreNodeDead()
     {
-        
+        if (_currentHealth <= 0)
+        {
+            IsDead = true;
+        }
+        return IsDead;
+    }
+    
+    
+    public void OnCollisionEnter(Collision col)
+    {
+        //TODO might have to reconsider how enemy tags work
+        if (col.gameObject.tag.Contains("Enemy"))
+        {
+            GameObject.Destroy(col.gameObject);
+            Hit = true;
+        }
     }
 }
